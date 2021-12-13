607624634ac07284729c1dcad206221ded825e37
roblebob <robert.koppisch@gmail.com>
2021-12-13 16:10:31 +0100

    finished TensorFlow Tutorial 2 - Tensor Basics

--- ML/TensorFlow/Basics/tutorial2-tensorbasics.py
+++ ML/TensorFlow/Basics/tutorial2-tensorbasics.py
@@ -1,37 +1,37 @@
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# to avoid unnecessary logs
 
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
 
 import tensorflow as tf
 
-# Initialization
-x = tf.constant(4, shape=(1, 1), dtype=tf.float32)
-print(x)
 
-x = tf.constant([[1, 2, 3], [4, 5, 6]], shape=(2, 3))
-print(x)
+physical_device = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_device[0], True)
+# this prevents tf to allocate all the GPU's memory
+# just in case of word errors
 
-x = tf.eye(3)
-print(x)
 
-x = tf.ones((4, 3))
-print(x)
 
-x = tf.zeros((3, 2, 5))
-print(x)
+# Initialization of Tensor
+x = tf.constant(4, shape=(1,1), dtype=tf.float32)
+x = tf.constant([[1, 2, 3], [4, 5, 6]])
 
-x = tf.random.uniform((2, 2), minval=0, maxval=1)
-print(x)
+x = tf.ones((3, 3))
+x = tf.eye(3) # identical matrix
 
-x = tf.random.normal((3, 3), mean=0, stddev=1)
-print(tf.cast(x, dtype=tf.float64))
-# tf.float (16,32,64), tf.int (8, 16, 32, 64), tf.bool
+x = tf.random.normal((3,3), mean=0, stddev=1)
+x = tf.random.uniform((1, 3), minval=0, maxval=1)
 
-x = tf.range(9)
-x = tf.range(start=0, limit=10, delta=2)
-print(x)
+x = tf.range(start=1, limit=10, delta=2)
+x = tf.cast(x, dtype=tf.float64)
 
-# Math
+
+# Mathematical Operations
+
+  # ... elementwise
+
 x = tf.constant([1, 2, 3])
 y = tf.constant([9, 8, 7])
 
@@ -39,44 +39,55 @@ z = tf.add(x, y)
 z = x + y
 
 z = tf.subtract(x, y)
-z = x - y
+Z = x - y
 
 z = tf.divide(x, y)
-z = x / y
+Z = x / y
 
 z = tf.multiply(x, y)
 z = x * y
 
-z = tf.tensordot(x, y, axes=1)
-
 z = x ** 5
 
+
+
+  # ... others
+
+z = tf.tensordot(x, y, axes=1)
+z = tf.reduce_sum(x * y, axis=0)
+
+
 x = tf.random.normal((2, 3))
-y = tf.random.normal((3, 2))
+y = tf.random.normal((3, 4))
+
 z = tf.matmul(x, y)
 z = x @ y
 
-x = tf.random.normal((2, 2))
 
 # Indexing
+
 x = tf.constant([0, 1, 1, 2, 3, 1, 2, 3])
-print(x[:])
-print(x[1:])
-print(x[1:3])
-print(x[::2])
-print(x[::-1])
+# print(x[:])
+# print(x[1:])
+# print(x[1:3])
+# print(x[::2])  # every other
+# print(x[::-1])  # reverse order
 
 indices = tf.constant([0, 3])
-x_indices = tf.gather(x, indices)
+x_ind = tf.gather(x, indices)
 
-x = tf.constant([[1, 2], [3, 4], [5, 6]])
 
-print(x[0, :])
-print(x[0:2, :])
 
 # Reshaping
+
 x = tf.range(9)
+print(x)
 
 x = tf.reshape(x, (3, 3))
+print(x)
 
-x = tf.transpose(x, perm=[1, 0])
+z = tf.transpose(x, perm=[1, 0])
+print(z)
+
+z = tf.transpose(x, perm=[0, 1])
+print(z)
