6fb8aad25907460fb37eb53a50dd08fec9f6d081
roblebob <robert.koppisch@gmail.com>
2021-12-14 13:43:36 +0100

    finished TensorFlow Tutorial 3 - Neural Networks with Sequential and Functional API

--- ML/TensorFlow/Basics/tutorial3-neuralnetwork.py
+++ ML/TensorFlow/Basics/tutorial3-neuralnetwork.py
@@ -1,47 +1,85 @@
 import os
-
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.datasets import mnist
 
-physical_devices = tf.config.list_physical_devices("GPU")
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+physical_device = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_device[0], True)
+# this prevents tf to allocate all the GPU's memory;  not necessary just in case of gpu errors
+
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+# x_train:  (60000, 28, 28)   uint8
+# x_test:   (10000, 28, 28)   uint8
+# y_train:  (60000,)          uint8
+# y_test:   (10000,)          uint8
+
 x_train = x_train.reshape(-1, 28 * 28).astype("float32") / 255.0
 x_test = x_test.reshape(-1, 28 * 28).astype("float32") / 255.0
+# x_train:  (60000, 784)
+# x_test:   (10000, 784)
 
-# Sequential API (Very convenient, not very flexible)
+
+
+
+
+
+
+# Sequential API (Very convient, not very flexible)
 model = keras.Sequential(
     [
-        keras.Input(shape=(28 * 28)),
-        layers.Dense(512, activation="relu"),
-        layers.Dense(256, activation="relu"),
-        layers.Dense(10),
+        keras.Input(shape=(28*28)),
+        layers.Dense(512, activation='relu', name='fist_layer'),
+        layers.Dense(256, activation='relu', name='second_layer'),
+        layers.Dense(10)
     ]
 )
 
 model = keras.Sequential()
-model.add(keras.Input(shape=(784)))
-model.add(layers.Dense(512, activation="relu"))
-model.add(layers.Dense(256, activation="relu", name="my_layer"))
+model.add(keras.Input(shape=(28*28)))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(256, activation='relu'))
 model.add(layers.Dense(10))
 
-# Functional API (A bit more flexible)
-inputs = keras.Input(shape=(784))
-x = layers.Dense(512, activation="relu", name="first_layer")(inputs)
-x = layers.Dense(256, activation="relu", name="second_layer")(x)
-outputs = layers.Dense(10, activation="softmax")(x)
-model = keras.Model(inputs=inputs, outputs=outputs)
+print(model.summary())
+
 
 model.compile(
-    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=keras.optimizers.Adam(lr=0.001),
+    metrics=["accuracy"],
+)
+
+
+
+model.fit(x_train, y_train, batch_size=32, epochs=5, verbose=2)
+model.evaluate(x_test, y_test, batch_size=32, verbose=2)
+
+
+
+
+
+
+# Functional API (A bit more flexible)
+inputs = keras.Input(shape=784, name="input")
+x = layers.Dense(512, activation='relu', name='first_layer')(inputs)
+x = layers.Dense(256, activation='relu', name='second_layer')(x)
+outputs = layers.Dense(10, activation='softmax', name='output')(x)
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+print(model.summary())
+
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False),  # false because output layer's activation is already softmax
     optimizer=keras.optimizers.Adam(lr=0.001),
     metrics=["accuracy"],
 )
 
 model.fit(x_train, y_train, batch_size=32, epochs=5, verbose=2)
 model.evaluate(x_test, y_test, batch_size=32, verbose=2)
+
+
