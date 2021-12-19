7f9dd2cbd0e07283827130673b3bc6e514676310
roblebob <robert.koppisch@gmail.com>
2021-12-19 13:47:35 +0100

    finished TensorFlow Tutorial 6 - RNNs, GRUs, LSTMs and Bidirectionality

--- ML/TensorFlow/Basics/tutorial6-rnn-gru-lstm.py
+++ ML/TensorFlow/Basics/tutorial6-rnn-gru-lstm.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
@@ -11,50 +12,67 @@ physical_devices = tf.config.list_physical_devices("GPU")
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-# x_train = x_train.reshape(-1, 784).astype("float32") / 255.0
-# x_test = x_test.reshape(-1, 784).astype("float32") / 255.0
-x_train = x_train.reshape([-1, 28, 28]).astype("float32") / 255.0
-x_test = x_test.reshape([-1, 28, 28]).astype("float32") / 255.0
+x_train = x_train.reshape(-1, 28, 28).astype("float32") / 255.0
+x_test = x_test.reshape(-1, 28, 28).astype("float32") / 255.0
+
+print(x_train.shape)
+
 
 model = keras.Sequential()
 model.add(keras.Input(shape=(None, 28)))
-model.add(layers.SimpleRNN(512, return_sequences=True, activation="relu"))
-model.add(layers.SimpleRNN(512, activation="relu"))
+# (None):represents the 28 timestamps;
+# (28):represents pixels at each timestamp
+
+model.add(layers.SimpleRNN(256, return_sequences=True, activation="relu"))
+# (return_sequences=True): returns output at each timestamp, and therefore another Layer can be stacked upon
+
+model.add(layers.SimpleRNN(256, activation="relu"))
+# here only at the last timestamp, the output is passed to the next layer
 model.add(layers.Dense(10))
+
 
 model = keras.Sequential()
 model.add(keras.Input(shape=(None, 28)))
-model.add(layers.SimpleRNN(256, return_sequences=True, activation="tanh"))
-model.add(layers.SimpleRNN(256))
+model.add(layers.GRU(256, return_sequences=True, activation="tanh"))
+model.add(layers.GRU(256, activation="tanh"))
 model.add(layers.Dense(10))
+
 
 model = keras.Sequential()
 model.add(keras.Input(shape=(None, 28)))
-model.add(layers.GRU(256, return_sequences=True, activation="relu"))
-model.add(layers.GRU(256))
+model.add(layers.LSTM(256, return_sequences=True, activation="tanh"))
+model.add(layers.LSTM(256, activation="tanh"))
 model.add(layers.Dense(10))
+
+
 
 model = keras.Sequential()
 model.add(keras.Input(shape=(None, 28)))
 model.add(
-    layers.Bidirectional(layers.LSTM(256, return_sequences=True, activation="relu"))
+    layers.Bidirectional(
+        layers.LSTM(256, return_sequences=True, activation="tanh")
+    )
 )
-model.add(layers.LSTM(256, name="lstm_layer2"))
+model.add(
+    layers.Bidirectional(
+        layers.LSTM(256, activation="tanh")
+    )
+)
 model.add(layers.Dense(10))
 
-model = keras.Sequential()
-model.add(keras.Input(shape=(None, 28)))
-model.add(
-    layers.Bidirectional(layers.LSTM(256, return_sequences=True, activation="relu"))
-)
-model.add(layers.Bidirectional(layers.LSTM(256, name="lstm_layer2")))
-model.add(layers.Dense(10))
+
+
+
+
+
+
 
 print(model.summary())
+
 model.compile(
     loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     optimizer=keras.optimizers.Adam(lr=0.001),
-    metrics=["accuracy"],
+    metrics=['accuracy']
 )
 
 model.fit(x_train, y_train, batch_size=64, epochs=10, verbose=2)
